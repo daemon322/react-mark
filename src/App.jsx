eb50c879f1214c77ec3c173fea502128f44326a1
daemon322 <131091382+daemon322@users.noreply.github.com>
2024-12-19 11:28:31 -0500

    App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,8 @@
 import Home from "./pages/Home";
-import CartPage from "./pages/CartPage";
-
 function App() {
   return (
       <div className="flex flex-col min-h-screen">
         <Home />
-        <CartPage />
       </div>
   );
 }
